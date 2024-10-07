6070a2643a2538615f02d057a0c651ea6c1ec203
Ryan Attard <ryanattard@gmail.com>
2024-10-07 10:57:34 +0200

    lesson 1

---
+++ SWD62AEP2024/Presentation/Controllers/StudentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
